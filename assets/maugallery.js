919374a730fa065444e3b6ab9c653b7e39ed38f2
SylvainRousset <sylvaindebisca@hotmail.fr>
2024-09-03 17:14:45 +0200

    next prev btn img height modal

--- assets/maugallery.js
+++ assets/maugallery.js
@@ -150,7 +150,7 @@
 
       $(imagesCollection).each(function(i) {
         if ($(activeImage).attr("src") === $(this).attr("src")) {
-          index = i ;
+          index = i -1 ;
         }
       });
       next =
@@ -192,7 +192,7 @@
           index = i;
         }
       });
-      next = imagesCollection[index] || imagesCollection[0];
+      next = imagesCollection[index +1] || imagesCollection[0];
       $(".lightboxImage").attr("src", $(next).attr("src"));
     },
     createLightBox(gallery, lightboxId, navigation) {
